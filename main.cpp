533e091e515d9fd7bdf28bf29d5293781ed6f07e
emmanuel.a <emmanuel.a@lycee-de-beauvoir.education>
2025-11-18 15:44:51 +0100

    Etape 3,4 et 5

--- main.cpp
+++ main.cpp
@@ -8,9 +8,7 @@
 * @class BTS CIEL 2
 * @date 18/11/2025
 *********************************************************************************************/
-
 using namespace std;
-
 /**
  * @brief affiche le menu principal de l'application
  */
@@ -26,13 +24,59 @@ void afficherMenu() {
     cout << "0 - Sortir du programme" << endl;
 }
 /**
+ * @brief fonction qui demande à l'utilisateur de choisir dans le menu avec des switch/case
+ * @param choix du numéro sélectionné
+ */
+void traiterChoix(int choix){
+    switch (choix){
+        case 1:
+            cout << "\n Traitement de l'option 1 : Affichage log sudo" << endl;
+            //  lire et afficher les logs sudo.
+            break;
+        case 2:
+            cout << "\n Traitement de l'option 2 : Affichage et enregistrement log ssh" << endl;
+            //  lire, afficher et sauvegarder les logs ssh.
+            break;
+        case 3:
+            cout << "\n Traitement de l'option 3 : pocoGetAllLog (Recuperation tous les logs via POCO)" << endl;
+            //  fonction POCO pour récupérer tous les logs.
+            break;
+        case 4:
+            cout << "\n Traitement de l'option 4 : pocoGetOneLog (Recuperation un log specifique via POCO)" << endl;
+            //  fonction POCO pour récupérer un log.
+            break;
+        case 5:
+            cout << "\n Traitement de l'option 5 : pocoPostSSHLog (Envoye un log SSH via POCO)" << endl;
+            //  fonction POCO pour poster un log SSH.
+            break;
+        case 0:
+            cout << "\n Arret du programme. Au revoir !" << endl;
+            break;
+        default:
+            cout << "\n Option " << choix << " FAUX !. Veuillez choisir une option valide (0-5)." << endl;
+            break;
+    }
+}
+
+/**
  * @brief affiche le titre de l'application du programme
  * @return
  */
 int main(){
     cout << "CIEL - Gestion centralisee des logs" << endl;
     afficherMenu();
-    return 0;
+    int choix;
+    cout << "Quelle est votre choix?" << endl;
+    cin >> choix;
 
+    //gestion des erreurs de saisie
+    if (cin.fail()){
+        cin.clear();
+        cin.ignore(1000, '\n');
+        cout << ">>> ERREUR : vous devez ecrire un nombre uniquement !" << endl;
+        return 0;
+    }
+    traiterChoix(choix);
+    return 0;
 }
 
