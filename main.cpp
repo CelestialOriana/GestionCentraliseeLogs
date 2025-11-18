a8be5e7bb29e8c2317b74b5371480285a8073231
emmanuel.a <emmanuel.a@lycee-de-beauvoir.education>
2025-11-18 15:05:15 +0100

    Etape 1

--- main.cpp
+++ main.cpp
@@ -1,9 +1,21 @@
 #include <iostream>
+/*********************************************************************************************
+* @file main.cpp
+* @brief Programme principal de l'application "Gestion centralisée des logs
+* @brief Ce programme gère l'affichage d'un menu et le traitement du choix de son utilisateur
+* @author Ah-hong Emmanuel
+* @version v1.0
+* @class BTS CIEL 2
+* @date 18/11/2025
+*********************************************************************************************/
 
 using namespace std;
-
+/**
+ * @brief affiche le titre de l'application du programme
+ * @return
+ */
 int main(){
-    cout << "Modele de projet" << endl;
+    cout << "CIEL - Gestion centralisee des logs" << endl;
     return 0;
 }
 
