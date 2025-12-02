1670f59cbe734c827b8bb4e97e2bd7d46e6fd6e3
emmanuel.a <emmanuel.a@lycee-de-beauvoir.education>
2025-12-02 16:22:01 +0100

    Le menu.cpp et menu.h modifié avec l'ajout de la fonction Documentation, testEcritureFichier() testLectureFichier()

--- Menu/menu.cpp
+++ Menu/menu.cpp
@@ -1,39 +1,29 @@
-/*********************************************************************************************
-* @file main.h
-* @brief déclaration des fonctions d'affichage et du traitement du menu
-* @author Ah-hong Emmanuel
-* @version v1.0
-* @class BTS CIEL 2
-* @date 18/11/2025
-*********************************************************************************************/
+/**
+* @file menu.cpp
+* @brief Définitions des fonctions du module Menu.
+*/
 
 #include <iostream>
 #include "menu.h"
-
-/*********************************************************************************************
-* @file menu.cpp
-* @brief Définitions des fonctions du module Menu.
-*********************************************************************************************/
+#include "../Logs/log.h"   // accès aux fonctions testEcritureFichier / testLectureFichier
 
 using namespace std;
 
 void afficherMenu() {
     cout << endl;
-    cout << "===== MENU PRINCIPAL =======" << endl;
-    cout << "1 - Afficher log sudo" << endl;
-    cout << "2 - Afficher et enregistrer log ssh" << endl;
+    cout << "===== MENU PRINCIPAL =====" << endl;
+    cout << "1 - Test d'ecriture dans un fichier texte" << endl;
+    cout << "2 - Test de lecture d'un fichier texte" << endl;
     cout << "3 - pocoGetAllLog" << endl;
     cout << "4 - pocoGetOneLog" << endl;
     cout << "5 - pocoPostSSHLog" << endl;
     cout << "0 - Sortir du programme" << endl;
 }
 
-/**
- * @brief Lit le choix de l'utilisateur, contrôle la saisie, et traite directement l'action.
- */
 void choisirLog() {
     int choix;
-    cout << "Quelle est votre choix ?" << endl;
+
+    cout << "Quelle est votre choix ? " << endl;
     cin >> choix;
 
     // Gestion d'erreur de saisie
@@ -44,26 +34,30 @@ void choisirLog() {
         return;
     }
 
-    // Traitement du choix
     switch (choix) {
         case 1:
-            cout << "\nTraitement de l'option 1 : Affichage log sudo" << endl;
+            cout << "\nTraitement de l'option 1 : Test d'ecriture dans un fichier texte" << endl;
+            testEcritureFichier();   // crée/écrase test.txt et écrit des lignes
             break;
 
         case 2:
-            cout << "\nTraitement de l'option 2 : Affichage et enregistrement log ssh" << endl;
+            cout << "\nTraitement de l'option 2 : Test de lecture d'un fichier texte" << endl;
+            testLectureFichier();    // lit et affiche test.txt
             break;
 
         case 3:
             cout << "\nTraitement de l'option 3 : pocoGetAllLog" << endl;
+            // TODO: ajouter l'appel POCO plus tard
             break;
 
         case 4:
             cout << "\nTraitement de l'option 4 : pocoGetOneLog" << endl;
+            // TODO: ajouter l'appel POCO plus tard
             break;
 
         case 5:
             cout << "\nTraitement de l'option 5 : pocoPostSSHLog" << endl;
+            // TODO: ajouter l'appel POCO plus tard
             break;
 
         case 0:
--- Menu/menu.h
+++ Menu/menu.h
@@ -1,17 +1,9 @@
-/*********************************************************************************************
-* @file main.h
-* @brief déclaration des fonctions d'affichage et du traitement du menu
-* @author Ah-hong Emmanuel
-* @version v1.0
-* @class BTS CIEL 2
-* @date 18/11/2025
-*********************************************************************************************/
-
-#pragma once
-/*********************************************************************************************
+/**
 * @file menu.h
 * @brief Déclarations des fonctions du module Menu (affichage et gestion des choix).
-*********************************************************************************************/
+*/
+
+#pragma once
 
 /**
  * @brief Affiche le menu principal de l'application.
@@ -20,10 +12,5 @@ void afficherMenu();
 
 /**
  * @brief Demande le choix utilisateur, contrôle la saisie et exécute immédiatement l'action.
- *
- * Cette fonction combine l'ancienne logique :
- *  - lecture du choix
- *  - gestion des erreurs
- *  - switch/case pour traiter les options
  */
 void choisirLog();
